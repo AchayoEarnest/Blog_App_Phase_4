5ff68ac2aab93973a6a44f36e975a8b18e63f8d4
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-12 00:02:50 +0300

    partitioning the article page into 3 sections

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :article
-    include Visible
+    # include Visible
 end
--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Articles.css'
 
 function Articles() {
   return (
-    <div>Articles</div>
+    <div className='article_page'>
+      <div className='left_nav'>
+        Left
+      </div>
+      <div className='middle_sect'>
+        center
+      </div>
+      <div className='right_sect'>
+        Right
+      </div>
+    </div>
   )
 }
 
