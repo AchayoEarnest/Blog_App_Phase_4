8a796adbcfb0fc8098caaa82d6847eba2f31891d
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-11 23:06:00 +0300

    added json output for index and show#actions in article controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_155419) do
+ActiveRecord::Schema.define(version: 2022_10_11_193701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "author"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commentor"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# puts "Seeding... "
+# Article.create(author: "Earnest", title: "The dead ocean", body: "This is part of the dangerous famous bermuda triangle")
+# Article.create(author: "James", title: "The Haunted Doll", body: "However much we've tried to through the doll away. It keeps coming back to the house")
+# Article.create(author: "John Bosko", title: "The death of magicians ", body: "Since most illusions by the magicians have been identified as mere trick. People started killing them hence they arent available anymore")
+
+# Comment.create(commentor: "James", body: "I love it", article_id: 1)
+# Comment.create(commentor: "Kennedy", body: "Awesome content", article_id: 2)
+# Comment.create(commentor: "John", body: "wonderful article", article_id: 2)
+# Comment.create(commentor: "Bosco", body: "So cool", article_id: 1)
+
+# puts "Done seeding"
