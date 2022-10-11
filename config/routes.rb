570a266ae4be0e0eeb94a8e584bdecf5b4f0bf84
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-11 20:43:29 +0300

    session create & destroy actions created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   # resources :users
   # Routing logic: fallback requests for React Router.
