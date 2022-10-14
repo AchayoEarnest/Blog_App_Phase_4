5d16d60897e3ea3181210065104da39df850da82
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-14 12:24:52 +0300

    seeding

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :commentor, :body
+  belongs_to :article
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :firstname, :lastname, :email, :password_digest
+  
+  has_many :articles
 end
